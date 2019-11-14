4ab57b16d71b7fd36e203b9171ba2943f7d27506
carlos <cf17carlos.yuste@iesjoandaustria.org>
2019-11-14 17:34:52 +0100

    make functional_tests an app, use LIveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -34,8 +34,9 @@ class NewVisitorTest(LiveServerTestCase):
         )
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
-        # is tying fly-fishing lures)
-        inputbox.send_keys('Use peacock feathers to make a fly')  
+       # is tying fly-fishing lures)
+        print("XXX abans d'enviar primer")
+        inputbox.send_keys('Buy peacock feathers')  
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
@@ -46,14 +47,16 @@ class NewVisitorTest(LiveServerTestCase):
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
+        print("XXX enviat primer")
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
        	time.sleep(1) 
+        print("XXX enviat segon")
 
    		# The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers 	to make a fly')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         self.fail('Finish the test!')  
 
